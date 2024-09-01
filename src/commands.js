4aa3e28f370f46ed66dcf1f42e2d2174f3998d7a
mca63 <mca63@cornell.edu>
2024-08-31 20:03:07 -0400

    added a few more shortcuts

--- src/commands.js
+++ src/commands.js
@@ -46,38 +46,36 @@ export type ColumnDataTableType = {|
 |};
 
 export const COMMANDS: { [CommandNames]: CommandType } = {
-  fb: {
-    name: "Facebook",
-    url: "https://facebook.com/",
-    searchurl: "https://www.facebook.com/search/top/?q=",
+  g: {
+    name: "Google",
+    url: "https://google.com/",
+    searchurl: "https://www.google.com/search?q=",
     category: "Search",
   },
-  mw: {
-    name: "Messenger Web",
-    url: "https://www.messenger.com/",
-    category: "Social Media",
-  },
-  wa: {
-    name: "WhatsApp Desktop App",
-    url: "whatsapp://",
-    category: "Social Media",
-  },
-  waw: {
-    name: "WhatsApp Web",
-    url: "https://web.whatsapp.com/",
-    category: "Social Media",
+  gm_personal: {
+    name: "Gmail - Personal",
+    url: "https://mail.google.com/mail/u/0/#inbox",
+    category: "Mail",
   },
   gm: {
     name: "Gmail",
-    url: "https://mail.google.com/mail/u/0",
-    searchurl: "https://mail.google.com/mail/u/",
+    url: "https://mail.google.com/mail/u/1/#inbox",
     category: "Mail",
+  },
+  gd_personal: {
+    name: "Google Drive - Personal",
+    url: "https://drive.google.com/drive/u/0/my-drive",
+    category: "File Storage",
   },
   gd: {
     name: "Google Drive",
-    url: "https://drive.google.com/drive/u/0",
-    searchurl: "https://drive.google.com/drive/u/",
+    url: "https://drive.google.com/drive/u/1/my-drive",
     category: "File Storage",
+  },
+  schedule: {
+    name: "Notion Schedule",
+    url: "https://www.notion.so/484d0eea741c49e5a874f7ffb1dbaf87?v=2e068ad6eae54dd98e1b0b859d2e1159",
+    category: "Productivity",
   },
   yt: {
     name: "YouTube",
@@ -85,19 +83,13 @@ export const COMMANDS: { [CommandNames]: CommandType } = {
     searchurl: "https://www.youtube.com/results?search_query=",
     category: "Search",
   },
-  gh: {
-    name: "GitHub",
-    url: "https://github.com/",
-    searchurl: "https://www.github.com/search?q=",
-    category: "Programming",
-  },
-  r: {
+  redd: {
     name: "Reddit",
     url: "https://reddit.com/",
     searchurl: "https://www.reddit.com/search?q=",
     category: "Social Media",
   },
-  l: {
+  linked: {
     name: "Linkedin",
     url: "https://linkedin.com/",
     category: "Social Media",
@@ -108,28 +100,10 @@ export const COMMANDS: { [CommandNames]: CommandType } = {
     searchurl: "https://instagram.com/",
     category: "Social Media",
   },
-  tw: {
-    name: "Twitter",
-    url: "https://twitter.com/",
-    searchurl: "https://twitter.com/search?q=",
-    category: "Social Media",
-  },
-  g: {
-    name: "Google",
-    url: "https://google.com/",
-    searchurl: "https://www.google.com/search?q=",
-    category: "Search",
-  },
-  n: {
+  nflix: {
     name: "Netflix",
     url: "https://netflix.com/",
     searchurl: "https://www.netflix.com/search?q=",
-    category: "Streaming",
-  },
-  pv: {
-    name: "Amazon Prime Video",
-    url: "https://www.amazon.co.uk/Amazon-Video/",
-    searchurl: "https://www.amazon.co.uk/s?i=instant-video&ref=nb_sb_noss_2&k=",
     category: "Streaming",
   },
   wiki: {
@@ -137,42 +111,6 @@ export const COMMANDS: { [CommandNames]: CommandType } = {
     url: "https://en.wikipedia.org",
     searchurl: "https://en.wikipedia.org/wiki/",
     category: "Search",
-  },
-  kz: {
-    name: "Kaizen",
-    url: "http://kaizenep.com",
-    category: "Medical",
-  },
-  nhs: {
-    name: "NHS Portal",
-    url: "https://portal.nhs.net",
-    category: "Medical",
-  },
-  ol: {
-    name: "Outlook",
-    url: "https://outlook.office.com/mail/",
-    category: "Mail",
-  },
-  db: {
-    name: "Dribbble",
-    url: "https://dribbble.com",
-    searchurl: "https://dribbble.com/search/",
-    category: "Design",
-  },
-  al: {
-    name: "Anaesthetic Logbook",
-    url: "https://forms.gle/sqQX8zA1te85BZ1H8",
-    category: "Medical",
-  },
-  bl: {
-    name: "jchui bunnylol",
-    url: "https://jchui.github.io/bunnylol",
-    category: "Search",
-  },
-  pw: {
-    name: "Patchwork Health",
-    url: "https://app.patchwork.health/shifts",
-    category: "Medical",
   },
   DEFAULT: {
     name: "Default - Google Search",
